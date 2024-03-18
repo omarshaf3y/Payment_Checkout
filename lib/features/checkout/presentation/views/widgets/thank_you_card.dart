eedeef831dd5812c1049f6de31394e41b2fe84e7
Omar <omarshaf3y99@gmail.com>
2024-03-18 22:10:44 +0200

    Create payment item info

--- lib/features/checkout/presentation/views/widgets/thank_you_card.dart
+++ lib/features/checkout/presentation/views/widgets/thank_you_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payment_checkout/core/utils/styles.dart';
+import 'package:payment_checkout/features/checkout/presentation/views/widgets/payment_item_info.dart';
 
 class ThankYouCard extends StatelessWidget {
   const ThankYouCard({
@@ -8,10 +10,49 @@ class ThankYouCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       decoration: ShapeDecoration(
         color: const Color(0xFFd9d9d9),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50 + 16, left: 22, right: 22),
+        child: Column(
+          children: [
+            Text(
+              'Thank You!',
+              textAlign: TextAlign.center,
+              style: Styles.style25,
+            ),
+            Text(
+              'Your transaction was successful',
+              textAlign: TextAlign.center,
+              style: Styles.style20,
+            ),
+            const SizedBox(
+              height: 42,
+            ),
+            const PaymentItemInfo(
+              title: 'Date',
+              value: '03/18/2024',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'Time',
+              value: '10:07 PM',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'To',
+              value: 'Omar Soliman',
+            ),
+          ],
         ),
       ),
     );
